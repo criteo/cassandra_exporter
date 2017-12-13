185de94a35ac6cff7c6872821bd1175bc7ebd746
Romain GÉRARD <r.gerard@criteo.com>
2017-12-13 16:33:39 +0100

    + Change token separator from / to : + Change scrape frequency from minute to seconds

--- src/main/java/com/criteo/nosql/cassandra/exporter/Main.java
+++ src/main/java/com/criteo/nosql/cassandra/exporter/Main.java
@@ -3,12 +3,8 @@ package com.criteo.nosql.cassandra.exporter;
 import io.prometheus.client.exporter.HTTPServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.impl.SimpleLogger;
 
-import javax.management.ObjectName;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 
 public class Main {
@@ -29,7 +25,7 @@ public class Main {
         HTTPServer server = new HTTPServer(cfg.getListenPort());
 
 
-        JmxScraper scrapper = new JmxScraper(String.format("service:jmx:rmi:///jndi/rmi://%s/jmxrmi", cfg.getHost()), cfg.getUser(), cfg.getPassword(), cfg.getSSL(), cfg.getBlacklist(), cfg.getMaxScrapFrequencyInMin());
+        JmxScraper scrapper = new JmxScraper(String.format("service:jmx:rmi:///jndi/rmi://%s/jmxrmi", cfg.getHost()), cfg.getUser(), cfg.getPassword(), cfg.getSSL(), cfg.getBlacklist(), cfg.getMaxScrapFrequencyInSec());
 
         boolean isOneShot = Arrays.asList(args).contains("--oneshot");
         if (isOneShot) {
