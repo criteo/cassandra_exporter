7d26551b6521f928d27572f443085ea0de1693a1
Romain GÉRARD <r.gerard@criteo.com>
2017-12-27 13:44:21 +0100

    Cleanup and add javadoc

--- src/main/java/com/criteo/nosql/cassandra/exporter/JmxScraper.java
+++ src/main/java/com/criteo/nosql/cassandra/exporter/JmxScraper.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.management.MBeanAttributeInfo;
 import javax.management.MBeanServerConnection;
-import javax.management.ObjectInstance;
 import javax.management.ObjectName;
 import javax.management.openmbean.CompositeData;
 import javax.management.openmbean.CompositeType;
@@ -18,6 +17,7 @@ import javax.naming.Context;
 import javax.rmi.ssl.SslRMIClientSocketFactory;
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -39,7 +39,6 @@ public class JmxScraper {
     private final List<Pattern> blacklist;
     private final TreeMap<Integer, List<Pattern>> scrapFrequencies;
     private final Map<Integer, Long> lastScrapes;
-    private final List<MBeanInfo> mBeanInfos;
     private final Map<String, Object> jmxEnv;
 
 
@@ -48,7 +47,6 @@ public class JmxScraper {
         this.blacklist = blacklist.stream().map(Pattern::compile).collect(toList());
         this.scrapFrequencies = new TreeMap<>();
         this.lastScrapes = new HashMap<>(scrapFrequencies.size());
-        this.mBeanInfos = new ArrayList<>();
 
         scrapFrequencies.forEach((k, v) -> {
             this.scrapFrequencies.put(k * 1000, v.stream().map(Pattern::compile).collect(toList()));
@@ -70,10 +68,36 @@ public class JmxScraper {
     }
 
     /**
-      * Get a list of mbeans on host_port and scrape their values.
-      *
-      * Values are passed to the receiver in a single thread.
-      */
+     * Convert a Cassandra EstimatedHistogram value into known percentiles histogram
+     *
+     * @param counts the serialized value of the estimatedhistrogram
+     * @return Percentiles of the histogram in the given order double[]{0.5, 0.75, 0.95, 0.98, 0.99};
+     */
+    private static double[] metricPercentilesAsArray(long[] counts) {
+        // Copy-pasted mostly from https://github.com/apache/cassandra/blob/f59df2893b66b3a8715b9792679e51815982a542/src/java/org/apache/cassandra/tools/NodeProbe.java#L1223
+        double[] result = new double[7];
+
+        if (counts == null || counts.length == 0) {
+            Arrays.fill(result, Double.NaN);
+            return result;
+        }
+
+        long[] offsets = EstimatedHistogram.newOffsets(counts.length, false);
+        EstimatedHistogram metric = new EstimatedHistogram(offsets, counts);
+
+        if (metric.isOverflowed()) {
+            logger.error("EstimatedHistogram overflowed larger than {}, unable to calculate percentiles", offsets[offsets.length - 1]);
+            for (int i = 0; i < result.length; i++)
+                result[i] = Double.NaN;
+        } else {
+            for (int i = 0; i < offsetPercentiles.length; i++)
+                result[i] = metric.percentile(offsetPercentiles[i]);
+        }
+        result[5] = metric.min();
+        result[6] = metric.max();
+        return result;
+    }
+
     public void run(final boolean forever) throws Exception {
 
         try(JMXConnector jmxc = JMXConnectorFactory.connect(new JMXServiceURL(jmxUrl), jmxEnv)) {
@@ -81,11 +105,8 @@ public class JmxScraper {
 
             do {
                 final long now = System.currentTimeMillis();
-                for(ObjectInstance name: beanConn.queryMBeans(null, null)) {
-                    collectMBeanInfos(beanConn, name.getObjectName());
-                }
-
-                mBeanInfos.stream()
+                beanConn.queryMBeans(null, null).stream()
+                        .flatMap(objectInstance -> toMBeanInfos(beanConn, objectInstance.getObjectName()))
                         .filter(m -> shouldScrap(m, now))
                         .forEach(mBean -> updateMetric(beanConn, mBean));
 
@@ -96,7 +117,6 @@ public class JmxScraper {
 
                 final long duration = System.currentTimeMillis() - now;
                logger.info("Scrap took {}ms for the whole run", duration);
-               this.mBeanInfos.clear();
 
                // don't go lower than 10 sec
                if(forever) Thread.sleep(Math.max(scrapFrequencies.firstKey() - duration, 10 * 1000));
@@ -104,6 +124,13 @@ public class JmxScraper {
         }
     }
 
+    /**
+     * Return if we should scrap this MBean at a given point in time
+     *
+     * @param mBeanInfo Should we scrap this MBean
+     * @param now       timestamp representing the point in time
+     * @return True if we should scrap it, False if not
+     */
     private boolean shouldScrap(final MBeanInfo mBeanInfo, final long now) {
         if(blacklist.stream().anyMatch(pattern -> pattern.matcher(mBeanInfo.metricName).matches())) {
             return false;
@@ -119,21 +146,31 @@ public class JmxScraper {
         return false;
     }
 
-    private void collectMBeanInfos(MBeanServerConnection beanConn, ObjectName mbeanName) {
+    /**
+     * Return all possible MbeanInfo from a MBean path.
+     * The main interest is to unroll of the attributes of an Mbeans in order to filter on it
+     *
+     * @param beanConn  The JMX connexion
+     * @param mbeanName The MBean path
+     */
+    private Stream<MBeanInfo> toMBeanInfos(MBeanServerConnection beanConn, ObjectName mbeanName) {
         try {
-            for(MBeanAttributeInfo attr: beanConn.getMBeanInfo(mbeanName).getAttributes()) {
-                if (!attr.isReadable()) {
-                    logger.debug("Cannot read attribute {} of {}", attr.getName(), mbeanName);
-                    continue;
-                }
-                final String name = getMetricPath(mbeanName, attr);
-                this.mBeanInfos.add(new MBeanInfo(name,  mbeanName, attr));
-            }
+            return Arrays.stream(beanConn.getMBeanInfo(mbeanName).getAttributes())
+                    .filter(MBeanAttributeInfo::isReadable)
+                    .map(mBeanAttributeInfo -> new MBeanInfo(getMetricPath(mbeanName, mBeanAttributeInfo), mbeanName, mBeanAttributeInfo));
         } catch (Exception e) {
             logger.error(" Error when scraping mbean {}", mbeanName, e);
+            return Stream.empty();
         }
     }
 
+    /**
+     * Return the formatted metric for a given MBean and attribute
+     *
+     * @param mbeanName Mbean object
+     * @param attr Specific attribute of this Mbean
+     * @return the formatted metric name
+     */
     private String getMetricPath(ObjectName mbeanName, MBeanAttributeInfo attr) {
         String properties = PATTERN.matcher(mbeanName.toString())
                 .replaceAll(metricSeparator)
@@ -141,6 +178,12 @@ public class JmxScraper {
         return properties.toLowerCase();
     }
 
+    /**
+     * Update the metrics registry for a given MBeanInfo
+     *
+     * @param beanConn
+     * @param mBeanInfo
+     */
     private void updateMetric(MBeanServerConnection beanConn, MBeanInfo mBeanInfo) {
 
         long start = System.currentTimeMillis();
@@ -154,6 +197,7 @@ public class JmxScraper {
             return;
         }
 
+        // Converting attribute to double
         switch (mBeanInfo.attribute.getType()) {
             case "long":
             case "int":
@@ -224,32 +268,9 @@ public class JmxScraper {
         logger.trace("Scrapping took {}ms for {}", (System.currentTimeMillis() - start), mBeanInfo.metricName);
     }
 
-
-    // Copy-pasted https://github.com/apache/cassandra/blob/f59df2893b66b3a8715b9792679e51815982a542/src/java/org/apache/cassandra/tools/NodeProbe.java#L1223
-    private static double[] metricPercentilesAsArray(long[] counts) {
-        double[] result = new double[7];
-
-        if (counts == null || counts.length == 0) {
-            Arrays.fill(result, Double.NaN);
-            return result;
-        }
-
-        long[] offsets = EstimatedHistogram.newOffsets(counts.length, false);
-        EstimatedHistogram metric = new EstimatedHistogram(offsets, counts);
-
-        if (metric.isOverflowed()) {
-            logger.error("EstimatedHistogram overflowed larger than {}, unable to calculate percentiles", offsets[offsets.length - 1]);
-            for (int i = 0; i < result.length; i++)
-                result[i] = Double.NaN;
-        } else {
-            for (int i = 0; i < offsetPercentiles.length; i++)
-                result[i] = metric.percentile(offsetPercentiles[i]);
-        }
-        result[5] = metric.min();
-        result[6] = metric.max();
-        return result;
-    }
-
+    /**
+     * POJO to hold information regarding a metric
+     */
     private static class MBeanInfo {
         public final String metricName;
         public final ObjectName mBeanName;
