25287f593881314c8c432527652aebd91c9df84b
Deimosfr <deimos@deimos.fr>
2018-04-17 13:56:29 +0200

    feat: adding java options to docker image

--- docker/run.sh
+++ docker/run.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+export JVM_OPTS="${JVM_OPTS:-Xmx}"
 export CASSANDRA_EXPORTER_CONFIG_host="${CASSANDRA_EXPORTER_CONFIG_host:-localhost:7199}"
 export CASSANDRA_EXPORTER_CONFIG_ssl="${CASSANDRA_EXPORTER_CONFIG_ssl:-False}"
 export CASSANDRA_EXPORTER_CONFIG_user="${CASSANDRA_EXPORTER_CONFIG_user}"
@@ -9,6 +10,7 @@ export CASSANDRA_EXPORTER_CONFIG_password="${CASSANDRA_EXPORTER_CONFIG_password}
 export CASSANDRA_EXPORTER_CONFIG_listenPort="${CASSANDRA_EXPORTER_CONFIG_listenPort:-8080}"
 
 echo "Starting Cassandra exporter"
+echo "JVM_OPTS: $JVM_OPTS"
 while IFS='=' read -r name value ; do
   if [[ $name == 'CASSANDRA_EXPORTER_CONFIG_'* ]]; then
     val="${!name}"
@@ -18,4 +20,4 @@ while IFS='=' read -r name value ; do
   fi
 done < <(env)
 
-/sbin/dumb-init /usr/bin/java -jar /opt/cassandra_exporter/cassandra_exporter.jar /etc/cassandra_exporter/config.yml
+/sbin/dumb-init /usr/bin/java ${JVM_OPTS} -jar /opt/cassandra_exporter/cassandra_exporter.jar /etc/cassandra_exporter/config.yml
